0a0d5070f02b38f4ed8b265445fe1257126905e6
Niyaz <niyaz434@gmail.com>
2018-04-09 01:43:31 +0530

    Fixed issues in click events

--- Moviez/app/src/main/java/com/example/mohamedniyaz/moviezapp/modules/Movie.java
+++ Moviez/app/src/main/java/com/example/mohamedniyaz/moviezapp/modules/Movie.java
@@ -37,6 +37,16 @@ public class Movie {
     @SerializedName("vote_average")
     private Double voteAverage;
 
+    private Boolean isFavourite =false ;
+
+
+    public Boolean getFavourite() {
+        return isFavourite;
+    }
+
+    public void setFavourite(Boolean favourite) {
+        isFavourite = favourite;
+    }
 
     public Movie(String posterPath, boolean adult, String overview, String releaseDate, List<Integer> genreIds, Integer id, String originalTitle, String originalLanguage, String title, String backdropPath, Double popularity, Integer voteCount, Boolean video, Double voteAverage) {
         this.posterPath = posterPath;
