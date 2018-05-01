25c26ba2edbe0d41dff3eaf188412b23e60603e7
Niyaz <niyaz434@gmail.com>
2018-05-01 14:29:43 +0530

    Sqlite database storage is done

--- Moviez/app/src/main/java/com/example/mohamedniyaz/moviezapp/database/SqliteHelper.java
+++ Moviez/app/src/main/java/com/example/mohamedniyaz/moviezapp/database/SqliteHelper.java
@@ -1,5 +1,6 @@
 package com.example.mohamedniyaz.moviezapp.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,7 +14,7 @@ public class SqliteHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME =  "MovieDatabase.db";
 
     //Table details
-    private static final String TABLE_NAME = "FAVURITE MOVIES";
+    private static final String TABLE_NAME = "FAVOURITE_MOVIES";
     private static final String COLUMN_MOVIE_ID = "MOVIE_ID";
     private static final String COLUMN_MOVIE_NAME = "MOVIE_NAME";
     private static final String COLUMN_MOVIE_ISFAVOURITE = "IS_FAVOURITE";
@@ -28,10 +29,10 @@ public class SqliteHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
     final String movieSqliteDatabase  = "CREATE TABLE " + TABLE_NAME + " ( "
-                                            + COLUMN_MOVIE_ID + "INTEGER, "
-                                            + COLUMN_MOVIE_NAME + "TEXT, "
-                                            + COLUMN_MOVIE_ISFAVOURITE + "INTEGER DEFAULT 0 "
-                                            + ") ";
+                                            + COLUMN_MOVIE_ID + " INTEGER, "
+                                            + COLUMN_MOVIE_NAME + " TEXT, "
+                                            + COLUMN_MOVIE_ISFAVOURITE + " INTEGER DEFAULT 0 "
+                                            + " ) ";
 
         db.execSQL(movieSqliteDatabase);
     }
@@ -41,7 +42,37 @@ public class SqliteHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
     }
 
+    public void insert(int movie_id, String movie_name, boolean isFavourite){
+        try{
+            SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
 
+            contentValues.put(COLUMN_MOVIE_ID,movie_id);
+            contentValues.put(COLUMN_MOVIE_NAME,movie_name);
+            contentValues.put(COLUMN_MOVIE_ISFAVOURITE,isTrue(isFavourite));
 
+            sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void update( int movie_id, boolean isFavourite){
+        try{
+            SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COLUMN_MOVIE_ISFAVOURITE,isTrue(isFavourite));
+            sqLiteDatabase.update(TABLE_NAME,contentValues,COLUMN_MOVIE_ID+"="+movie_id,null);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public int isTrue (boolean isFavourite){
+        return isFavourite ? 1 : 0 ;
+    }
 
 }
---
+++ Moviez/app/src/main/java/com/example/mohamedniyaz/moviezapp/fragments/MovieResponseFragment.java
@@ -0,0 +1,167 @@
+package com.example.mohamedniyaz.moviezapp.fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mohamedniyaz.moviezapp.R;
+import com.example.mohamedniyaz.moviezapp.activity.MovieIdActivity;
+import com.example.mohamedniyaz.moviezapp.modules.AdapterModel;
+import com.example.mohamedniyaz.moviezapp.modules.GenereClass;
+import com.example.mohamedniyaz.moviezapp.modules.MovieId;
+import com.example.mohamedniyaz.moviezapp.modules.SpokenClass;
+import com.example.mohamedniyaz.moviezapp.rest.ApiClient;
+import com.example.mohamedniyaz.moviezapp.rest.ApiInterface;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.ContentValues.TAG;
+
+public class MovieResponseFragment extends Fragment{
+    TextView title,description,rating,rating_count,genre,language;
+    SimpleDraweeView fresco_image;
+    ArrayList<AdapterModel> arrayList = new ArrayList<>();
+
+    Uri uri = Uri.parse("https://image.tmdb.org/t/p/w500/" );
+    private final static String API_KEY = "0e12101a22c608993caa890e9dabea92";
+    public int movieId;
+
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_two,container,false);
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)view.findViewById(R.id.collapsingToolbar);
+
+        final FloatingActionButton fab;
+        final boolean[] flag = {true}; // true if first icon is visible, false if second one is visible.
+
+        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(flag[0]){
+
+                    fab.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_favourite));
+                    flag[0] = false;
+
+                }else {
+
+                    fab.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_favourite_border));
+                    flag[0] = true;
+
+                }
+
+            }
+        });
+
+
+
+
+
+
+        description = (TextView)view.findViewById(R.id.description_text);
+        rating  = (TextView)view.findViewById(R.id.rating_text);
+        rating_count = (TextView)view.findViewById(R.id.rating_count_text);
+        genre = (TextView)view.findViewById(R.id.genre_text);
+        fresco_image = (SimpleDraweeView)view.findViewById(R.id.toolbarImage) ;
+        language = (TextView)view.findViewById(R.id.language_text);
+
+
+        Intent intent = getActivity().getIntent();
+        int idNo = intent.getIntExtra("Int",0);
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MovieId> call = apiService.getMovieDetails(idNo,API_KEY);
+        call.enqueue(new Callback<MovieId>() {
+            @Override
+            public void onResponse(Call<MovieId> call, Response<MovieId> response) {
+
+                String title_name = response.body().getOriginal_title();
+                Log.d(TAG, "Title name: "+title_name);
+                String overview = response.body().getOverview();
+                float vote_average = response.body().getVote_average();
+                ArrayList<GenereClass> genereClasses = (ArrayList<GenereClass>) response.body().getGenres();
+                ArrayList<SpokenClass> spokenClasses = (ArrayList<SpokenClass>) response.body().getSpoken_languages();
+                int vote_count  = response.body().getVote_count();
+                String backdrop_path = response.body().getBackdropPath();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                StringBuilder stringBuilder1 = new StringBuilder();
+
+
+                    collapsingToolbarLayout.setTitle(title_name);
+                    description.setText(overview);
+                    rating.setText(" "+vote_average);
+                    rating_count.setText(" "+vote_count);
+                    fresco_image.setImageURI(uri + backdrop_path);
+
+
+                    System.out.println("Nope: "+overview);
+                    System.out.println("Array"+genereClasses);
+                    for(int j = 0;j<genereClasses.size();j++){
+                        if (stringBuilder.length() > 0 ) {
+                            stringBuilder.append(", ");
+                        }
+
+                        stringBuilder.append(genereClasses.get(j).getName().toString());
+                        System.out.println("String Builder"+stringBuilder.toString());
+                        System.out.println("ArrayIn "+genereClasses.get(j).getName().toString());
+                    }
+
+                    for(int k =0; k < spokenClasses.size();k++){
+                        if(stringBuilder1.length() >0){
+                            stringBuilder1.append(", ");
+                        }
+                        stringBuilder1.append(spokenClasses.get(k).getName().toString());
+
+                    }
+
+                    stringBuilder.append('.');
+                    stringBuilder1.append('.');
+                    genre.setText(stringBuilder.toString());
+                    language.setText(stringBuilder1.toString());
+
+                Log.d(TAG, "onResponse: ");
+
+            }
+
+            @Override
+            public void onFailure(Call<MovieId> call, Throwable t) {
+                Log.d(TAG, "onFailure: ");
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+
+
+
+        return view;
+    }
+}
